e76a13faa0c40269bd0711782367818033ab4c57
dcodez23 <71770133+dcodez23@users.noreply.github.com>
2023-06-08 21:10:28 -0400

    Test file upload metadata

--- FrontEnd/S3.tf
+++ FrontEnd/S3.tf
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_object" "object" {
   for_each = fileset("./WebSiteFiles/", "**")
   key      = each.value
   source   = "./WebSiteFiles/${each.value}"
-  content_type = "text/html"
+  #content_type = "text/html"
   #coalesce(var.content_types[regex("(\\.[^.]+)$", each.value)[0]], var.default_content_type)
 
   etag = filemd5("./WebSiteFiles/${each.value}")
